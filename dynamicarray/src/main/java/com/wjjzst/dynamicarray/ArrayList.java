a87c47f08d67e7f2d3f3db3276684f023a9926a5
wjj <www.wzzst310@163.com>
2019-04-15 08:30:12 +0800

    可变数组

---
+++ dynamicarray/src/main/java/com/wjjzst/dynamicarray/ArrayList.java
@@ -0,0 +1,21 @@
+package com.wjjzst.dynamicarray;
+
+public class ArrayList<T> implements List<T> {
+    private T[] elements;
+    private int size;
+
+    @Override
+    public void add(T t) {
+
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
--- dynamicarray/src/main/java/com/wjjzst/dynamicarray/List.java
+++ dynamicarray/src/main/java/com/wjjzst/dynamicarray/List.java
@@ -2,7 +2,10 @@ package com.wjjzst.dynamicarray;
 
 public interface List<T> {
     void add(T t);
+
     void remove(int index);
+
     void clear();
+
 
 }
