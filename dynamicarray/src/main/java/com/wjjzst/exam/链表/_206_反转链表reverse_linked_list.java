d88267bb6f054ae020115e2f51a63416ff4651dc
wjj <www.wzzst310@163.com>
2019-04-19 01:07:50 +0800

    单向链表

--- dynamicarray/src/main/java/com/wjjzst/exam/链表/_206_反转链表reverse_linked_list.java
+++ dynamicarray/src/main/java/com/wjjzst/exam/链表/_206_反转链表reverse_linked_list.java
@@ -1,15 +1,36 @@
 package com.wjjzst.exam.链表;
 
 /**
- *
  * https://leetcode-cn.com/problems/reverse-linked-list/
+ *
  * @Author: Wjj
  * @Date: 2019-04-19 00:33
  */
 public class _206_反转链表reverse_linked_list {
-    class Solution {
+    static class Solution {
         public ListNode reverseList(ListNode head) {
-            return null;
+            while (head != null) {
+                ListNode temp = head.next;
+                head.next = head;
+                head = temp;
+                head = head.next;
+            }
+            return head;
         }
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        Solution solution = new Solution();
+        ListNode listNode = solution.reverseList(node1);
+        System.out.println(listNode.val);
     }
 }
