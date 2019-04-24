ab1a0ade0677138c510aeae4f515853281782ed1
wjj <www.wzzst310@163.com>
2019-04-25 01:42:03 +0800

    栈题目

---
+++ dynamicarray/src/main/java/com/wjjzst/exam/二叉树/TreeNode.java
@@ -0,0 +1,14 @@
+package com.wjjzst.exam.二叉树;
+
+/**
+ * @Author: Wjj
+ * @Date: 2019-04-25 01:40
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
