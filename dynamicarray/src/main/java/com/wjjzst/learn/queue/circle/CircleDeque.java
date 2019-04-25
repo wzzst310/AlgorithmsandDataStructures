681db9a584b44ed303c57ef53d94e58eac35ae43
wjj <www.wzzst310@163.com>
2019-04-26 02:07:04 +0800

    队列以及循环队列

---
+++ dynamicarray/src/main/java/com/wjjzst/learn/queue/circle/CircleDeque.java
@@ -0,0 +1,84 @@
+package com.wjjzst.learn.queue.circle;
+
+
+/**
+ * 数组方式实现单向循环队列
+ *
+ * @Author: Wjj
+ * @Date: 2019-04-26 00:05
+ */
+public class CircleDeque<E> {
+    private int front;
+    private int size;
+    private E[] elements;
+    public static final int DEFAULE_CAPACIEY = 10;
+
+    public CircleDeque() {
+        elements = (E[]) new Object[DEFAULE_CAPACIEY];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void enQueueFront(E element) {
+        ensureCapacity(size + 1);
+        front = index(-1);
+        elements[front] = element;
+        size++;
+    }
+
+    public E deQueueFront() {
+        E frontElement = elements[front];
+        elements[front] = null;
+        front = index(-1);
+        size--;
+        return frontElement;
+    }
+
+    public void enQueueRear(E element) {
+        ensureCapacity(size + 1);
+        elements[index(size)] = element;
+        size++;
+    }
+
+    public E deQueueRear() {
+        int rearIndex = index(size - 1 );
+        E rear = elements[index(rearIndex)];
+        elements[rearIndex] = null;
+        size--;
+        return rear;
+    }
+
+    public E front() {
+        return elements[front];
+    }
+
+    public E rear() {
+        return elements[index(-1)];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private int index(int index) {
+        index = front + index;
+        return index - (index < elements.length ? 0 : index - elements.length);
+    }
+
+    private void ensureCapacity(int capacity) {
+        int oldCapacity = elements.length;
+        if (oldCapacity >= capacity) {
+            return;
+        }
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        E[] newElements = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[index(i)];
+        }
+        front = 0;
+        elements = newElements;
+        System.out.println("扩容为" + newCapacity);
+    }
+}
---
+++ dynamicarray/src/main/java/com/wjjzst/learn/queue/circle/CircleQueue.java
@@ -0,0 +1,68 @@
+package com.wjjzst.learn.queue.circle;
+
+
+import sun.security.util.Length;
+
+/**
+ * 数组方式实现单向循环队列
+ *
+ * @Author: Wjj
+ * @Date: 2019-04-26 00:05
+ */
+public class CircleQueue<E> {
+    private int front;
+    private int size;
+    private E[] elements;
+    public static final int DEFAULE_CAPACIEY = 10;
+
+    public CircleQueue() {
+        elements = (E[]) new Object[DEFAULE_CAPACIEY];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void enQueue(E element) {
+        ensureCapacity(size);
+        elements[index(size)] = element;
+        size++;
+    }
+
+
+    public E deQueue() {
+        E frontElement = elements[front];
+        elements[front] = null;
+        front = index(1);
+        size--;
+        return frontElement;
+    }
+
+    public E front() {
+        return elements[front];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private int index(int index) {
+        index = front + index;
+        return index - (index < elements.length ? 0 : index - elements.length);
+    }
+
+    private void ensureCapacity(int capacity) {
+        int oldCapacity = elements.length;
+        if (oldCapacity >= capacity) {
+            return;
+        }
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        E[] newElements = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[index(i)];
+        }
+        front = 0;
+        elements = newElements;
+        System.out.println("扩容为" + newCapacity);
+    }
+}
